0d56274f24b8b7c351d570e866895340e38bf14e
Vahid Reza Asadi <vahidreza.asadi@gmail.com>
2018-05-21 00:07:03 +0430

    some models added

--- models/price.js
+++
@@ -1,7 +0,0 @@
-class Price {
-    constructor(basePrice, rentPrice, sellPrice) {
-        this.basePrice = basePrice;
-        this.rentPrice = rentPrice;
-        this.sellPrice = sellPrice;
-    }
-}
